de5fd7611e76e5a1c24f5b001dce3d24e43046ba
Matheus Fernando Golmini Albuquerque de Souza <matheusgolminif@gmail.com>
2022-08-16 22:28:39 -0300

    test config

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,6 @@ import 'package:flutter/foundation.dart';
 
 void main() async {
   await Hive.initFlutter();
-  final ios = defaultTargetPlatform == TargetPlatform.iOS;
-  var app_secret = ios
-      ? "123cfac9-123b-123a-123f-123273416a48"
-      : "00f371a4-3e86-44b5-a4aa-6aeeaba7bade";
-
-  await AppCenter.start(
-      app_secret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
 
   runApp(
     MultiProvider(
@@ -34,8 +27,50 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ios = defaultTargetPlatform == TargetPlatform.iOS;
+  String _appSecret = "3f1f3b0e-24ff-436a-b42d-3c08b117d46a";
+  String _installId = 'Unknown';
+  bool _areAnalyticsEnabled = false, _areCrashesEnabled = false;
+
+  _MyAppState() {
+    final ios = defaultTargetPlatform == TargetPlatform.iOS;
+    _appSecret = ios
+        ? "a8a33033-ef2f-4911-a664-a7d118287ce7"
+        : "3f1f3b0e-24ff-436a-b42d-3c08b117d46a";
+  }
+
+  @override
+  initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    await AppCenter.start(
+        _appSecret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
+
+    if (!mounted) return;
+
+    var installId = await AppCenter.installId;
+
+    var areAnalyticsEnabled = await AppCenterAnalytics.isEnabled;
+    var areCrashesEnabled = await AppCenterCrashes.isEnabled;
+
+    setState(() {
+      _installId = installId;
+      _areAnalyticsEnabled = areAnalyticsEnabled;
+      _areCrashesEnabled = areCrashesEnabled;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
